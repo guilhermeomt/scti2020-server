bdf2597204c5140c55f622bb7608692d47b02243
Guilherme Tavares <guilhermet213@gmail.com>
2020-07-08 11:20:06 -0300

    Complete CRUD endpoints for User controller/routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 
+// ! Refactor this entire controller, but for now it works
+
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
@@ -22,9 +24,66 @@ exports.createUser = async (req, res) => {
     // TODO: Request body validation
     const newUser = await User.create(req.body);
 
-    res.status(200).json({
+    res.status(201).json({
       status: 'success',
       user: newUser,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    res.status(200).json({
+      status: 'success',
+      user,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      user: updatedUser,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    res.status(200).json({
+      status: 'success',
+      user: deletedUser,
     });
   } catch (err) {
     res.status(404).json({
