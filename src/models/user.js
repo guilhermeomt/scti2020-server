d06190e650f98185c3d48d70fdb72aa3914ff322
Guilherme Tavares <guilhermet213@gmail.com>
2020-07-14 18:06:17 -0300

    Add validator and update User schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,46 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator').default;
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, 'Informe o seu nome.'],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, 'Informe o seu sobrenome.'],
     },
     phone: {
       type: String,
-      required: true,
+      required: [true, 'Informe o seu número de telefone.'],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Informe o seu email.'],
       unique: true,
+      validate: [
+        {
+          validator(val) {
+            return isEmail(val);
+          },
+          message: 'O email inserido é inválido.',
+        },
+        {
+          validator(val) {
+            return mongoose
+              .model('User')
+              .findOne({ email: val })
+              .then((user) => !user);
+          },
+          message: 'Este email já está cadastrado. Tente outro.',
+        },
+      ],
     },
     password: {
       type: String,
-      required: true,
-      minlength: 6,
+      required: [true, 'Informe sua senha.'],
+      minlength: [8, 'A senha deve ter no mínimo 8 caracteres.'],
       select: false,
     },
     isAdmin: {
