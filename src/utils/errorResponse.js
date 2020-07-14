039ae81db62b2fa1dbe0902cde3d686fc6529e88
Guilherme Tavares <guilhermet213@gmail.com>
2020-07-14 17:11:27 -0300

    Update Error Response class with a bool to check if the error is operational
    Also correct default constructor message

--- src/utils/errorResponse.js
+++ src/utils/errorResponse.js
@@ -3,6 +3,7 @@ class ErrorResponse extends Error {
     super(message);
     this.status = 'error';
     this.statusCode = statusCode;
+    this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
   }
